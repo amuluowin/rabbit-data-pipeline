1d731dfe374ac6f715b4045ed4a3cb21f3edb930
huangconghua <995018884@qq.com>
2019-12-21 18:28:41 +0800

    fix bug

--- src/AbstractPlugin.php
+++ src/AbstractPlugin.php
@@ -102,7 +102,7 @@ abstract class AbstractPlugin extends BaseObject implements InitInterface
                 return true;
             }
         }
-        return true;
+        return false;
     }
 
     /**
