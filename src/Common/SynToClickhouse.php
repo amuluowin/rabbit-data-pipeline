29ae08c75251eee4f1b593ddc257b52f16d93d0d
Albert <amuluowin@163.com>
2021-03-08 16:14:56 +0800

    support last updatedAt to get new record

--- src/Common/SynToClickhouse.php
+++ src/Common/SynToClickhouse.php
@@ -13,6 +13,7 @@ class SynToClickhouse extends BaseSyncData
     protected ?string $equal;
     protected string $primary;
     protected bool $onlyInsert;
+    protected string $updatedAt;
 
     public function init(): void
     {
@@ -21,7 +22,8 @@ class SynToClickhouse extends BaseSyncData
             $this->equal,
             $this->primary,
             $this->onlyInsert,
-        ] = ArrayHelper::getValueByArray($this->config, ['equal', 'primary', 'onlyInsert'], ['onlyInsert' => false]);
+            $this->updatedAt
+        ] = ArrayHelper::getValueByArray($this->config, ['equal', 'primary', 'onlyInsert', 'updatedAt'], ['onlyInsert' => false]);
 
         if ($this->primary === null) {
             throw new InvalidConfigException('primary field is empty!');
@@ -48,13 +50,21 @@ class SynToClickhouse extends BaseSyncData
         }
         $on = rtrim($on, ' and ');
 
-        $sql = "INSERT INTO {$this->to} ({$this->field}" . ($this->onlyInsert ? ')' : ',flag)') . "
-        SELECT {$fields}" . ($this->onlyInsert ? '' : ',0 AS flag') . "
-          FROM {$this->from} f 
-          ANTI LEFT JOIN {$this->to} t on $on" . ($this->onlyInsert ? '' : "
-         WHERE ({$primary}) NOT IN(
-        SELECT {$this->primary} FROM {$this->to}
-         WHERE flag= 0)");
+        if ($this->updatedAt !== null) {
+            $sql = "INSERT INTO {$this->to} ({$this->field}" . ($this->onlyInsert ? ')' : ',flag)') . "
+            SELECT {$fields}" . ($this->onlyInsert ? '' : ',0 AS flag') . "
+            FROM {$this->from} f where f.{$this->updatedAt}>(SELECT max({$this->updatedAt}) from {$this->to} )";
+        } else {
+            $sql = "INSERT INTO {$this->to} ({$this->field}" . ($this->onlyInsert ? ')' : ',flag)') . "
+            SELECT {$fields}" . ($this->onlyInsert ? '' : ',0 AS flag') . "
+              FROM {$this->from} f 
+              ANTI LEFT JOIN {$this->to} t on $on" . ($this->onlyInsert ? '' : "
+             WHERE ({$primary}) NOT IN(
+            SELECT {$this->primary} FROM {$this->to}
+             WHERE flag= 0)");
+        }
+
+
         getDI('click')->get($this->db)->createCommand($sql)->execute();
 
         if (!$this->onlyInsert) {
