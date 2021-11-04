9c2a59aff501c79c8f3a876bbc44c051fb419fbf
Albert <amuluowin@163.com>
2021-11-04 21:14:24 +0800

    fix function

--- src/Senders/WorkerSender.php
+++ src/Senders/WorkerSender.php
@@ -23,7 +23,7 @@ class WorkerSender implements ISender
         ]);
         if (null === $server = ServerHelper::getServer()) {
             App::warning("Not running in server, use local process");
-            $ipc = create(CommonHandler::class)->handler($this, $ipc);
+            $ipc = create(CommonHandler::class)->handle($this, $ipc);
         } elseif (!$server instanceof Server) {
             throw new InvalidConfigException("only use for swoole_server");
         } else {
